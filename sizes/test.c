b045cc6ae6093014bb0e4bd76e71a10a34744cdc
norayr <norayr@arnet.am>
2015-05-20 23:04:33 +0800

    darwin

--- sizes/test.c
+++ sizes/test.c
@@ -13,28 +13,34 @@ printf("short int %i\n", sizeof(short int));
 printf("int %i\n", sizeof(int));
 printf("long %i\n", sizeof(long));
 
-printf("__jmp_buf  %i\n", sizeof(__jmp_buf));
-printf("__sigset_t  %i\n", sizeof(__sigset_t));
+//printf("__jmp_buf  %i\n", sizeof(__jmp_buf));
+//printf("__sigset_t  %i\n", sizeof(__sigset_t));
 printf("jmp_buf  %i\n", sizeof(jmp_buf));
 printf("sigjmp_buf  %i\n", sizeof(sigjmp_buf));
 /* for armv6 */
-typedef
-        struct __attribute__((__packed__)) My_JmpBuf {
+/*typedef
+        struct __attribute__((__packed__)) my_JmpBuf {
                 int jmpbuf[64];
                 int maskWasSaved;
                 int savedMask[32];
-        } My_JmpBuf;
-
-/* for x86_64
+        } my_JmpBuf;
+*/
+/* for x86_64 linux glibc
 typedef
-        struct My_JmpBuf {
+        struct my_JmpBuf {
                 long jmpbuf[8];
                 int maskWasSaved;
                 long savedMask[16];
-         } My_JmpBuf;
+         } my_JmpBuf;
 */
-printf("My_JmpBuf  %i\n", sizeof(My_JmpBuf));
 
+/* x86_64 darwin 
+typedef int jmp_buf[((9 * 2) + 3 + 16)];
+typedef int sigjmp_buf[((9 * 2) + 3 + 16) + 1];
+*/
+typedef int my_JmpBuf[((9 * 2) + 3 + 16)];
+printf("my_JmpBuf  %i\n", sizeof(my_JmpBuf));
+/*
 long n, m;
 jmp_buf j;
 
@@ -42,5 +48,6 @@ jmp_buf j;
 n=&j[0].__saved_mask;
 m=&j[0];
 printf("offset %i\n", n-m);
+*/
 }
 
